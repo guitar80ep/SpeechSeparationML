3298713b329cab8e5623db18647cc871a8782fd2
Ryan Niemocienski <rniemo@gmail.com>
2016-04-12 18:12:07 -0400

    Add test script process.m

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # SpeechSeparationML
 A machine learning example for AI 2 that separates speech.
+
+run from scratch:
+run main
+run process
+run train
+run tester
+
+run using saved data/models:
+run tester
+% the real data
+sound(data(1,:))
+% recovered sound data
+sound(output)
+
--- process.m
+++ process.m
@@ -1,15 +1,51 @@
-%[d,sr] = wavread(data(1,:));
-d = signal1;
-sr = 16000;
-tic; [D,F] = gammatonegram(d,sr,.025,.01,64); toc
-%Elapsed time is 0.140742 seconds.
-subplot(211)
-imagesc(20*log10(D)); axis xy
-caxis([-90 -30])
-colorbar
-% F returns the center frequencies of each band;
-% display whichever elements were shown by the autoscaling
-set(gca,'YTickLabel',round(F(get(gca,'YTick'))));
-ylabel('freq / Hz');
-xlabel('time / 10 ms steps');
-title('Gammatonegram - fast method')
+
+'loading data'
+load data;
+samples = 20;
+mixtures = zeros(samples,128,400);
+masks = zeros(samples,128,400);
+
+signal1 = data(1,:) * pow2(15);
+r1 = gammatoneFast(signal1);
+coch1 = cochleagram(r1);
+
+'generating samples'
+for x = 2:samples+1
+    signalx = data(x,:) * pow2(15);
+    rx = gammatoneFast(signalx);
+    cochx = cochleagram(rx);
+    mixtures(x-1,:,:) = coch1 + cochx;
+    masks(x-1,:,:) = coch1 > cochx;
+    x
+end
+
+'creating models'
+models = cell(128,400);
+for row = 1:128
+    for col = 1:400
+        x = mixtures(:,row,col);
+        y = masks(:,row,col);
+        models{row,col} = fitcsvm(x,y);
+    end
+    row
+end
+
+'creating test cochleagram'
+signal_test = data(30,:) * pow2(15);
+r_test = gammatoneFast(signal_test);
+coch_test = cochleagram(r_test);
+
+mixture_test = coch_test + coch1;
+
+'creating mask'
+generated_mask = zeros(128,400);
+for row = 1:128
+    for col = 1:400
+        model = models{row,col};
+        generated_mask(row,col) = predict(model, mixture_test);
+    end
+    row
+end
+
+'creating output'
+output = synthesisFast(mixture, generated_mask);
